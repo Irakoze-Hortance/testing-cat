b9afcbb4d1615a0eb653ec391668f1110045caba
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-22 21:48:01 +0200

    EmployeeRepository

--- src/main/java/com/example/springdatacrudtesting/Repository/EmployeeRepository.java
+++ src/main/java/com/example/springdatacrudtesting/Repository/EmployeeRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     Optional<Employee> findByEmail(String email);
+
+    Optional<Employee> findById(int id);
 }
