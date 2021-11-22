cfd353115f7577ff6315fed48c32ad6017a5a559
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-22 21:13:14 +0200

    api response class

---
+++ src/main/java/com/example/springdatacrudtesting/utils/APIResponse.java
@@ -0,0 +1,31 @@
+package com.example.springdatacrudtesting.utils;
+
+public class APIResponse {
+    private boolean status;
+    private String message;
+
+    public APIResponse(){
+        super();
+    }
+
+    public APIResponse(boolean status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
