b084e3080569966e810e7c7deab84bdc399a3aa0
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-22 21:49:33 +0200

    EmployeeService

--- src/main/java/com/example/springdatacrudtesting/services/EmployeeService.java
+++ src/main/java/com/example/springdatacrudtesting/services/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -20,21 +21,30 @@ public class EmployeeService {
             Employee employee=new Employee(firstName,lastName,email);
             return  employeeRepository.save(employee);
         }
-        public void deleteEmployee(Long id){
+        public void deleteEmployee(int id){
             employeeRepository.findById(id)
                     .orElseThrow(()->new RuntimeException("Employee not found with id" + id));
             employeeRepository.deleteById(id);
         }
 
-        public  Employee updateEmployee(Long id, Employee employee){
+        public  Employee updateEmployee(int id, Employee employee){
             employeeRepository.findById(id)
                     .orElseThrow(()->new RuntimeException("Employee not found with id " +id));
             employee.setId(id);
             return employeeRepository.save(employee);
         }
-        public Employee getEmployeeDetail(Long id){
+        public Employee getEmployeeDetail(int id){
             return  employeeRepository.findById(id)
                     .orElseThrow(()->new RuntimeException("Employee not found with id" +id));
+        }
+
+        public Employee getById(int id){
+            Optional<Employee> findByID=employeeRepository.findById(id);
+            if(findByID.isPresent()){
+                Employee employee=findByID.get();
+                return  employee;
+            }
+            return null;
         }
 
 }
